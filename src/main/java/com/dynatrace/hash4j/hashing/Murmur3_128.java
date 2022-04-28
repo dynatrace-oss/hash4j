43d99e4c97b13e0f508d03b5de83d8a861e6a9f6
Otmar Ertl <otmar.ertl@dynatrace.com>
2022-04-28 11:10:41 +0200

    removed shifts by 0

--- src/main/java/com/dynatrace/hash4j/hashing/Murmur3_128.java
+++ src/main/java/com/dynatrace/hash4j/hashing/Murmur3_128.java
@@ -93,10 +93,10 @@ class Murmur3_128 extends AbstractHashCalculator {
         case 10:
           k2 ^= (input[off + 9] & 0xFFL) << 8;
         case 9:
-          k2 ^= (input[off + 8] & 0xFFL) << 0;
+          k2 ^= input[off + 8] & 0xFFL;
           h2 ^= mixK2(k2);
         case 8:
-          k1 ^= (input[off + 7] & 0xFFL) << 56;
+          k1 ^= (long)input[off + 7] << 56;
         case 7:
           k1 ^= (input[off + 6] & 0xFFL) << 48;
         case 6:
@@ -110,7 +110,7 @@ class Murmur3_128 extends AbstractHashCalculator {
         case 2:
           k1 ^= (input[off + 1] & 0xFFL) << 8;
         case 1:
-          k1 ^= (input[off + 0] & 0xFFL) << 0;
+          k1 ^= input[off + 0] & 0xFFL;
           h1 ^= mixK1(k1);
       }
 
