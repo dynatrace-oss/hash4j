9ba4f08e801d3c3ef3f9c147b731b21c84ca3874
Michael Leichtfried <michael.leichtfried@dynatrace.com>
2022-09-06 10:11:03 +0200

    Reformat using the new black

--- python/benchmark_evaluation.py
+++ python/benchmark_evaluation.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-
 import time
 from collections import defaultdict, namedtuple
 from os import listdir
