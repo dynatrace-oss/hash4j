4932c9469c2de4cdf7e931303f6560c9d5da3f31
Otmar Ertl <otmar.ertl@dynatrace.com>
2023-06-14 17:17:02 +0200

    reduced visibility of abstract hash streams

--- src/main/java/com/dynatrace/hash4j/hashing/AbstractHashStream64.java
+++ src/main/java/com/dynatrace/hash4j/hashing/AbstractHashStream64.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Dynatrace LLC
+ * Copyright 2022-2023 Dynatrace LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package com.dynatrace.hash4j.hashing;
 import java.util.*;
 import java.util.function.ToLongFunction;
 
-public abstract class AbstractHashStream64 extends AbstractHashStream32 implements HashStream64 {
+abstract class AbstractHashStream64 extends AbstractHashStream32 implements HashStream64 {
 
   @Override
   public int getHashBitSize() {
