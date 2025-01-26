50bd0479125bef641f905fe40a41fff047db13f1
Otmar Ertl <otmar.ertl@dynatrace.com>
2025-01-26 14:46:38 +0100

    added xxh3_128 reference implementation and checksums, fixed use of deprecated openssl methods

--- reference-implementations/xxh3/xxh3_checksum_config.hpp
+++ reference-implementations/xxh3/xxh3_checksum_config.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Dynatrace LLC
+ * Copyright 2022-2025 Dynatrace LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 #define XXH3_CHECKSUM_CONFIG_HPP
 
 #include <string>
+#include <cstdint>
 
 class XXH3ChecksumConfig {
 
