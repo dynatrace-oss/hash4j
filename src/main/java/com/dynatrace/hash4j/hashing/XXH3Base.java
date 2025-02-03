f15110ad9ed919ad8d8d9e092a89c8eaae83052e
Otmar Ertl <otmar.ertl@dynatrace.com>
2025-02-03 11:02:04 +0100

    fixed license in XXH3Base

--- src/main/java/com/dynatrace/hash4j/hashing/XXH3Base.java
+++ src/main/java/com/dynatrace/hash4j/hashing/XXH3Base.java
@@ -13,6 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * This implementation was derived from
+ *
+ * https://github.com/OpenHFT/Zero-Allocation-Hashing/blob/zero-allocation-hashing-0.16/src/main/java/net/openhft/hashing/XXH3.java
+ *
+ * which was published under the license below:
+ *
+ * Copyright 2015 Higher Frequency Trading http://www.higherfrequencytrading.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.dynatrace.hash4j.hashing;
 
 import static com.dynatrace.hash4j.hashing.AbstractHasher.*;
