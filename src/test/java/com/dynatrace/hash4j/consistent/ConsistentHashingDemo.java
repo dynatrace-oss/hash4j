63ba3ffa3514b540d89c6541eeebcf6af001d7e4
Otmar Ertl <otmar.ertl@dynatrace.com>
2025-04-02 11:02:16 +0200

    renamed demo test

--- src/test/java/com/dynatrace/hash4j/consistent/ConsistentHashingDemo.java
+++ src/test/java/com/dynatrace/hash4j/consistent/ConsistentHashingDemo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 Dynatrace LLC
+ * Copyright 2023-2025 Dynatrace LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import org.junit.jupiter.api.Test;
 class ConsistentHashingDemo {
 
   @Test
-  void demoJumphash() {
+  void demoJumpBackHash() {
 
     // create a consistent bucket hasher
     ConsistentBucketHasher consistentBucketHasher =
