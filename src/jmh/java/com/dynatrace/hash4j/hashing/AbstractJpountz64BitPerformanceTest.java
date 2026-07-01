b3e5a4e54edf70f10aa2fc72ef7ddc6ca4593dd6
Otmar Ertl <otmar.ertl@dynatrace.com>
2026-07-01 11:12:26 +0200

    added performance tests for net.jpountz.xxhash.XXHash[32|64]

---
+++ src/jmh/java/com/dynatrace/hash4j/hashing/AbstractJpountz64BitPerformanceTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2026 Dynatrace LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dynatrace.hash4j.hashing;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
+import net.jpountz.xxhash.XXHash64;
+import org.openjdk.jmh.infra.Blackhole;
+
+public abstract class AbstractJpountz64BitPerformanceTest extends AbstractPerformanceTest {
+
+  @Override
+  protected void hashBytesDirect(byte[] b, Blackhole blackhole) {
+    blackhole.consume(createHashFunction().hash(b, 0, b.length, 0L));
+  }
+
+  @Override
+  protected void hashBytesViaAccess(byte[] b, Blackhole blackhole) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  protected void hashCharsDirect(String s, Blackhole blackhole) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  protected void hashBytesIndirect(byte[] b, Blackhole blackhole) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  protected void hashCharsIndirect(String s, Blackhole blackhole) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  protected void hashCharsUTF8Indirect(String s, Blackhole blackhole) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  protected void hashObject(TestObject testObject, Blackhole blackhole) {
+    try {
+      ByteArrayDataOutput dataOutput = ByteStreams.newDataOutput();
+      testObject.writeToDataOutput(dataOutput);
+      hashBytesDirect(dataOutput.toByteArray(), blackhole);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected abstract XXHash64 createHashFunction();
+}
